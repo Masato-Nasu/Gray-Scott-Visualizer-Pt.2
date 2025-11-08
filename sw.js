2d05e420f2976eea3061f10ef6ed54ff2825a059
那須　雅人 <1bitexist@gmail.com>
2025-11-08 22:47:41 +0900

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE='imperfect-turing-v1.0.3';
-const ASSETS=['./','./index.html','./main.js','./manifest.json','./shaders/pass.vert','./shaders/sim.frag','./shaders/vis.frag','./icons/icon-192.png','./icons/icon-512.png'];
-self.addEventListener('install',e=>{ e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))); });
-self.addEventListener('activate',e=>{ e.waitUntil((async()=>{ const keys=await caches.keys(); await Promise.all(keys.map(k=>{ if(k!==CACHE) return caches.delete(k); })); self.clients.claim(); })()); });
-self.addEventListener('fetch',e=>{ e.respondWith((async()=>{ const r=await caches.match(e.request); if(r) return r; return fetch(e.request); })()); });
