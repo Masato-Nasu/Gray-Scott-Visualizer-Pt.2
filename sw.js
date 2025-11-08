133d02e2c413f31fe4eb54e3af2b37a8df0daf67
那須　雅人 <1bitexist@gmail.com>
2025-11-08 22:22:46 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='imperfect-turing-v1.0.4';
+const CACHE='imperfect-turing-v1.0.6';
 const ASSETS=['./','./index.html','./main.js','./manifest.json','./shaders/pass.vert','./shaders/sim.frag','./shaders/vis.frag','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',e=>{ e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))); });
 self.addEventListener('activate',e=>{ e.waitUntil((async()=>{ const keys=await caches.keys(); await Promise.all(keys.map(k=>{ if(k!==CACHE) return caches.delete(k); })); self.clients.claim(); })()); });
